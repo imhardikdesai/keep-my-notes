56a3785e81cc2e47f9d6995a1e6e6508de420d53
hardik <hp676913@gmail.com>
2022-03-26 17:55:55 +0530

    Working add title input

--- App.js
+++ App.js
@@ -3,28 +3,46 @@ showNotes();
 //set event listner in add button
 addNote.addEventListener('click', (e) => {
     let inpTxt = document.getElementById('inpTxt');
+    let inpTitle = document.getElementById('inpTitle');
+
     //here if-else is check for null alue in inpTxt  Box
     if (inpTxt.value == null || inpTxt.value == "") {
         alert("Enter Some Text to Add Note");
     } else {
 
         //This code is actual code to add note in LocalStorage 
+        let myTitle = localStorage.getItem("myTitle");
         let myNotes = localStorage.getItem("myNotes");
+        if (myTitle == null) {
+            titleObj = [];
+        } else {
+            titleObj = JSON.parse(myTitle);
+        }
         if (myNotes == null) {
             noteObj = [];
         } else {
             noteObj = JSON.parse(myNotes);
         }
-        noteObj.push(inpTxt.value)
-        localStorage.setItem('myNotes', JSON.stringify(noteObj))
+        noteObj.push(inpTxt.value);
+        titleObj.push(inpTitle.value);
+        localStorage.setItem('myTitle', JSON.stringify(titleObj));
+        localStorage.setItem('myNotes', JSON.stringify(noteObj));
         inpTxt.value = "";
+        inpTitle.value = "";
     }
     showNotes();
 });
 
 function showNotes() {
     let notes = document.getElementById('notes');
+    let myTitle = localStorage.getItem("myTitle");
     let myNotes = localStorage.getItem("myNotes");
+
+    if (myTitle == null) {
+        titleObj = [];
+    } else {
+        titleObj = JSON.parse(myTitle);
+    }
 
     if (myNotes == null) {
         noteObj = [];
@@ -32,36 +50,55 @@ function showNotes() {
         noteObj = JSON.parse(myNotes);
     }
     let html = "";
-    noteObj.forEach((element, index) => {
-        //concat html with all index
+    Object.keys(titleObj).forEach(function (element, key) {
         html += `
                     <div class="card mx-2 my-2" style="width: 18rem;">
                         <div class="card-body">
-                            <h5 class="card-title">Note ${index + 1}</h5>
-                            <p class="card-text">${element}</p>
-                            <button id="${index}" onclick="delNote(this.id);" class="btn btn-primary">Delete Note</button>
+                            <h5 class="card-title">${titleObj[element]}</h5>
+                            <p class="card-text">${noteObj[element]}</p>
+                            <button id="${noteObj[key]}" onclick="delNote(this.id);" class="btn btn-primary">Delete Note</button>
                         </div>
                    </div>
                 `;
     });
+
+    // noteObj.forEach((element, index) => {
+    //     //concat html with all index
+    //     html += `
+    //                 <div class="card mx-2 my-2" style="width: 18rem;">
+    //                     <div class="card-body">
+    //                         <h5 class="card-title">Note ${index + 1}</h5>
+    //                         <p class="card-text">${element}</p>
+    //                         <button id="${index}" onclick="delNote(this.id);" class="btn btn-primary">Delete Note</button>
+    //                     </div>
+    //                </div>
+    //             `;
+    // });
+
     // Retrive Div In HTML and add new html from localstorage
     if (noteObj.length != 0) {
         notes.innerHTML = html;
     } else {
         notes.innerHTML = `Nothing to Show, Enter "Some Notes" To Show Content 📒`;
     }
-
 }
 
 function delNote(index) {
     let myNotes = localStorage.getItem("myNotes");
+    let myTitle = localStorage.getItem("myTitle");
     if (myNotes == null) {
         noteObj = [];
     } else {
         noteObj = JSON.parse(myNotes);
     }
+    if (myTitle == null) {
+        titleObj = [];
+    } else {
+        titleObj = JSON.parse(myTitle);
+    }
 
     noteObj.splice(index, 1);
+    titleObj.splice(index, 1);
     localStorage.setItem("myNotes", JSON.stringify(noteObj));
     showNotes();
 }
